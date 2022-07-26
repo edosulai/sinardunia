185432aef3250b52f2374e16f475b42eba4bf00f
Edo Sulai <ngufeel@gmail.com>
2022-07-26 14:41:49 +0700

    Tambah file database

---
+++ sinardunia.sql
@@ -0,0 +1,367 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Jul 2022 pada 09.40
+-- Versi server: 8.0.28
+-- Versi PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sinardunia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cart`
+--
+
+CREATE TABLE `cart` (
+  `idcart` int NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int NOT NULL,
+  `tglorder` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(10) NOT NULL DEFAULT 'Cart'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `cart`
+--
+
+INSERT INTO `cart` (`idcart`, `orderid`, `userid`, `tglorder`, `status`) VALUES
+(28, '16siy42DYKeso', 1, '2022-07-26 07:15:17', 'Confirmed');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detailorder`
+--
+
+CREATE TABLE `detailorder` (
+  `detailid` int NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `idproduk` int NOT NULL,
+  `qty` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `detailorder`
+--
+
+INSERT INTO `detailorder` (`detailid`, `orderid`, `idproduk`, `qty`) VALUES
+(35, '16siy42DYKeso', 60, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int NOT NULL,
+  `namakategori` varchar(20) DEFAULT 'NULL'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `namakategori`) VALUES
+(1, 'Alat Tulis'),
+(2, 'Buku Tulis'),
+(3, 'Meja Belajar');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konfirmasi`
+--
+
+CREATE TABLE `konfirmasi` (
+  `idkonfirmasi` int NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int NOT NULL,
+  `payment` varchar(10) NOT NULL,
+  `namarekening` varchar(25) NOT NULL,
+  `tglbayar` date NOT NULL,
+  `tglsubmit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `konfirmasi`
+--
+
+INSERT INTO `konfirmasi` (`idkonfirmasi`, `orderid`, `userid`, `payment`, `namarekening`, `tglbayar`, `tglsubmit`) VALUES
+(12, '16siy42DYKeso', 1, 'GOPAY', 'Sulai', '2022-07-12', '2022-07-26 07:16:01');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `userid` int NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `tgljoin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(7) NOT NULL DEFAULT 'Member',
+  `lastlogin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`userid`, `namalengkap`, `email`, `password`, `notelp`, `alamat`, `tgljoin`, `role`, `lastlogin`) VALUES
+(1, 'Admin', 'admin', '$2y$10$GJVGd4ji3QE8ikTBzNyA0uLQhiGd6MirZeSJV1O6nUpjSVp1eaKzS', '01234567890', 'Indonesia', '2020-03-16 11:31:17', 'Admin', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `no` int NOT NULL,
+  `metode` varchar(25) NOT NULL,
+  `norek` varchar(25) NOT NULL,
+  `logo` text,
+  `an` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`no`, `metode`, `norek`, `logo`, `an`) VALUES
+(4, 'GOPAY', '+6289637739631', 'images/gopay.jpg', 'GOPAY AL'),
+(5, 'Bank BCA', '123123123', 'images/bca.jpg', 'BCA ASD'),
+(6, 'DANA', '+6289637739631', 'images/dana.png', 'DANA AL');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `idproduk` int NOT NULL,
+  `idkategori` int DEFAULT NULL,
+  `namaproduk` varchar(100) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  `deskripsi` varchar(200) DEFAULT NULL,
+  `rate` int DEFAULT NULL,
+  `hargabefore` int DEFAULT NULL,
+  `hargaafter` int DEFAULT NULL,
+  `stok` int DEFAULT NULL,
+  `ukuran` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `idkategori`, `namaproduk`, `gambar`, `deskripsi`, `rate`, `hargabefore`, `hargaafter`, `stok`, `ukuran`) VALUES
+(60, 1, 'Doll', 'produk/16G6xwafmUB3M.jpg', 'Just Doll For Happy', 4, 10000, 9000, 50, 'besar'),
+(61, 1, 'wadaw', 'produk/16Skh8zdtB3gg.jpg', 'awd', 3, 9000, 8000, 40, 'besar'),
+(62, 2, 'Headlight PUSH ON HL-20501', 'produk/169dDA3EJtsco.jpg', 'Pertanyaan 1', 3, 15000, 14000, 20, 'besar');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_data`
+--
+
+CREATE TABLE `tb_data` (
+  `id_data` int NOT NULL,
+  `nama_barang` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `x1` double NOT NULL,
+  `x2` double NOT NULL,
+  `x3` double NOT NULL,
+  `x4` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `keputusan` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_hasil`
+--
+
+CREATE TABLE `tb_hasil` (
+  `id_hasil` int NOT NULL,
+  `id_data` int NOT NULL,
+  `x1` double NOT NULL,
+  `x2` double NOT NULL,
+  `x3` double NOT NULL,
+  `x4` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `prediksi` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_hasil_pengujian`
+--
+
+CREATE TABLE `tb_hasil_pengujian` (
+  `id_hasil` int NOT NULL,
+  `id_data` int NOT NULL,
+  `x1` double NOT NULL,
+  `x2` double NOT NULL,
+  `x3` double NOT NULL,
+  `x4` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `prediksi` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`idcart`),
+  ADD UNIQUE KEY `orderid` (`orderid`),
+  ADD KEY `orderid_2` (`orderid`);
+
+--
+-- Indeks untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD PRIMARY KEY (`detailid`),
+  ADD KEY `orderid` (`orderid`),
+  ADD KEY `idproduk` (`idproduk`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indeks untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD PRIMARY KEY (`idkonfirmasi`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idproduk`);
+
+--
+-- Indeks untuk tabel `tb_data`
+--
+ALTER TABLE `tb_data`
+  ADD PRIMARY KEY (`id_data`);
+
+--
+-- Indeks untuk tabel `tb_hasil`
+--
+ALTER TABLE `tb_hasil`
+  ADD PRIMARY KEY (`id_hasil`);
+
+--
+-- Indeks untuk tabel `tb_hasil_pengujian`
+--
+ALTER TABLE `tb_hasil_pengujian`
+  ADD PRIMARY KEY (`id_hasil`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `idcart` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  MODIFY `detailid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  MODIFY `idkonfirmasi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `userid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `no` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idproduk` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD CONSTRAINT `idproduk` FOREIGN KEY (`idproduk`) REFERENCES `produk` (`idproduk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `cart` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `login` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
